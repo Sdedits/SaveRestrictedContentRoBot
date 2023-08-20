b3f3240f2f241710699e82a9043359cd2da171ec
Saurabh Dudhe <130128590+Sdedits@users.noreply.github.com>
2023-08-20 15:41:17 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("API_ID", 15391466, cast=int)
+API_HASH = config("API_HASH", 9388b7cc13f3a782b1d5bca493d3aae2)
+BOT_TOKEN = config("BOT_TOKEN", 6614754415:AAEarxzaVeNe33FiE524MllPBBSsfpJ-5MQ)
+SESSION = config("SESSION", BQBDDq4gLp-YdoeUpAhU3hVzIjoJmNh_j80OZzZfq4YcsKx6x7gpkzfVlOjJ0Rfv1H_J4gWMeFw3niDjNmXTpiRxxJJcDeqiQKroNetOJ-3rkbzMH2X4WGhFu_JHxr79Hlag0ImNge6nELrN5Li3qJWNUdOER1eNdQWiIVNSH-UlOOBVuAjACw78T7jwz0AeF7J9HuI63ZE_HfeadIP7UZFm3vMebEjk_zVBiR1Caf10yd1n6r1r8dEyjoaHmuZdQOYDQgTiP5XQEZZ1wRweeQFz6KyRSMwZaj9OG7yzlOybuSp_EmJXUewzLtrkpsCpeZSfJ8v2tNsJ9fE6Wf86a2weAAAAAWgSk1wA)
+FORCESUB = config("FORCESUB", Edu_News_Exams)
+AUTH = config("AUTH", 5396638898, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
